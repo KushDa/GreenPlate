dc6470e3a7246b8d56f97d69acc31d4d1cad1908
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-04-04 21:48:43 +0530

    changed the frontend

---
+++ App_front/capacitor_grad/app/new3.sh
@@ -0,0 +1,4 @@
+    git add .
+    git commit -m "Moved Android project to new_folder"
+    git push origin sus
+    
---
+++ App_front/capacitor_grad/app/src/main/java/com/greenplate/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.greenplate.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
